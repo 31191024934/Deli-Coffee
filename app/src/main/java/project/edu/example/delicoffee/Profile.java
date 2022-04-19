201465c9871da5935b2f94e9c5a7bfee7e521ec3
31191024934 <90551722+31191024934@users.noreply.github.com>
2022-04-19 16:34:18 +0700

    này nè

---
+++ app/src/main/java/project/edu/example/delicoffee/Profile.java
@@ -0,0 +1,15 @@
+package project.edu.example.delicoffee;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.LinearLayout;
+
+public class Profile extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
